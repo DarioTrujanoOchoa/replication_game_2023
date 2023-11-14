4dc708fa8f0992bb810a047e7daa6263b8352434
DarioTrujanoOchoa <dtrujano.22@gmail.com>
2023-11-14 15:08:00 -0800

    Update graphs from figure2.R

--- src/figure2.R
+++ src/figure2.R
@@ -8,9 +8,15 @@ library(pacman)
 p_load(tidyverse)
 p_load(ggplot2)
 p_load(cowplot)
+p_load(showtext)
+font_add_google("Schoolbell", "bell")
 
 # import data
 source("src/import_data.R")
+
+# for the graph sizes
+g = 1.618034
+s = 5
 
 # calculate rates of false reports
 data.bsr.qsr <-
@@ -40,7 +46,8 @@ period_prop_false_report %>%
   scale_x_continuous(breaks = 1:10 ) +
   theme_minimal_hgrid()
 
-ggsave("results/fig2A_original.pdf")
+ggsave("results/fig2A_original.pdf",
+       width=s*g, height=s, pointsize = s)
 
 ## figure 2B ----
 prior_prop_false_report <- 
@@ -56,7 +63,8 @@ prior_prop_false_report %>%
   ylab("Fraction of false reports") +
   theme_minimal_hgrid()
 
-ggsave("results/fig2B_original.pdf")
+ggsave("results/fig2B_original.pdf",
+       width=s*g, height=s, pointsize = s)
 
 # Learning for the 50% prior ----
 
@@ -77,22 +85,41 @@ bsr_info %>%
 
 # Proportion of false report for the prior 50% across order of presentation
 # There was no trend, it seems that the false report rate is constant.
+
 bsr_info %>% 
-  filter(pur == 50) %>% 
-  group_by(order_prior) %>% 
+  group_by(order_prior,pur) %>% 
   summarise(prop_false_report = mean(false_report)) %>% 
-  ggplot() + 
-  geom_line(aes(x = order_prior,y = prop_false_report)) +
-  ylim(0,1)
+  ggplot(aes(x = order_prior,
+             y = prop_false_report,
+             color = factor(pur))) + 
+  geom_line() + geom_point(size = 2) +
+  ylim(0,1) +
+  theme_minimal_hgrid() +
+  labs(
+    x = "Round", 
+    y = "Fraction of false reports", family = "bell") +
+  scale_color_discrete(name = "Prior") 
+
+ggsave("results/rounds_all_priors.pdf",
+       width=s*g, height=s, pointsize = s)
 
 # Prior Belief elicited.
 # The prior belief should be around 50, however, there is clear trend downwards
 bsr_info %>% 
-  filter(pur == 50) %>% 
-  group_by(order_prior) %>% 
+  group_by(order_prior,pur) %>% 
   summarise(prior_stated = mean(belief1)) %>% 
-  ggplot() + 
-  geom_line(aes(x = order_prior,y = prior_stated))
+  ggplot(aes(x = order_prior,y = prior_stated,
+             color = factor(pur))) + 
+  geom_line() + geom_point() +
+  ylim(20,80) +
+  theme_minimal_hgrid() +
+  labs(
+    x = "Round", 
+    y = "Stated Prior") +
+  scale_color_discrete(name = "Prior") 
+
+ggsave("results/rounds_stated_prior.pdf",
+       width=s*g, height=s, pointsize = s)
 
 # Filter for the first round per prior ----
 
@@ -121,7 +148,6 @@ period_prop_false_report_table %>%
                 group = rounds)) +
   ylim(0,1) +
   labs(
-    title = "Comparisson with the first round",
     x = "Period", 
     y = "Fraction of false reports") + 
   scale_x_continuous(breaks = 1:10 ) +
@@ -132,8 +158,8 @@ period_prop_false_report_table %>%
                       labels = c("All Rounds",
                                  "Round 1")) 
 
-
-ggsave("results/fig2A_round_one.pdf")
+ggsave("results/fig2A_round_one.pdf",
+       width=s*g, height=s, pointsize = s)
 
 ## figure 2B ----
 prior_prop_false_report_r1 <-
@@ -159,7 +185,6 @@ prior_prop_false_report_table %>%
            position=position_dodge()) +
   ylim(0,1) +
   labs(
-    title = "Comparisson with the first round",
     x = "Known prior of Red Urn",
     y = "Fraction of false reports") +
   theme_minimal_hgrid() +
@@ -169,5 +194,6 @@ prior_prop_false_report_table %>%
                        labels = c("All Rounds",
                                   "Round 1")) 
 
-ggsave("results/fig2B_round_one.pdf")
+ggsave("results/fig2B_round_one.pdf",
+       width=s*g, height=s, pointsize = s)
 
