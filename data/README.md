5d68decc1485f1c727afebcb18b3515ceae4c634
DarioTrujanoOchoa <dtrujano.22@gmail.com>
2023-11-14 13:02:24 -0800

    Edit the figure2.R to look better

--- data/README.md
+++ data/README.md
@@ -2,4 +2,4 @@
 
 The data in this folder is the same authors published in the original replication package published along the paper.
  
-The descriptionof the variables can be seen in the readme.pdf file.
+The description of the variables can be seen in the readme.pdf file.
--- src/figure2.R
+++ src/figure2.R
@@ -17,25 +17,21 @@ data.bsr.qsr <-
 data.bsr.qsr %>%
   mutate(false_report = (belief1 != pur))
 
-# data for BSR and information
-
-data.bsr.qsr %>% 
-  filter(scoringrule == 1, 
-         treatment == 1) %>% 
-  distinct(subjectid)
-
+# data for BSR (elicitation method) and information treatment
 bsr_info <-
 data.bsr.qsr %>% 
   filter(scoringrule == 1, 
          treatment == 1)
-
 # there are 60 participant in BSR-info condition
 bsr_info %>% distinct(subjectid)
 
 ## figure 2A ----
+period_prop_false_report <-
 bsr_info %>% 
   group_by(period) %>% 
-  summarise(prop_false_report = mean(false_report)) %>% 
+  summarise(prop_false_report = mean(false_report))
+
+period_prop_false_report %>% 
   ggplot() + 
   geom_line(aes(x = period,y = prop_false_report)) +
   ylim(0,1) +
@@ -47,9 +43,12 @@ bsr_info %>%
 ggsave("results/fig2A_original.pdf")
 
 ## figure 2B ----
+prior_prop_false_report <- 
 bsr_info %>% 
   group_by(pur) %>% 
-  summarise(prop_false_report = mean(false_report))%>% 
+  summarise(prop_false_report = mean(false_report))
+
+prior_prop_false_report %>% 
   ggplot() + 
   geom_col(aes(x = factor(pur),y = prop_false_report)) +
   ylim(0,1) +
@@ -67,7 +66,6 @@ bsr_info <-
   group_by(subjectid,pur) %>% 
   arrange(subjectid, period) %>% 
   mutate(order_prior = 1:n()) 
-
 
 # The number of rounds per prior is different for each prior.
 # 50% has a larger number of repetitions.
@@ -87,8 +85,8 @@ bsr_info %>%
   geom_line(aes(x = order_prior,y = prop_false_report)) +
   ylim(0,1)
 
-# Posterior Belief elicited.
-# The posterior belief should be around 50, however, there is clear trend downwards
+# Prior Belief elicited.
+# The prior belief should be around 50, however, there is clear trend downwards
 bsr_info %>% 
   filter(pur == 50) %>% 
   group_by(order_prior) %>% 
@@ -99,31 +97,77 @@ bsr_info %>%
 # Filter for the first round per prior ----
 
 ## figure 2A ----
+# filter the first period
+period_prop_false_report_r1 <-
 bsr_info %>% 
   filter(order_prior == 1) %>% 
   group_by(period) %>% 
-  summarise(prop_false_report = mean(false_report)) %>% 
-  ggplot() + 
-  geom_line(aes(x = period,y = prop_false_report)) +
-  ylim(0,1) +
-  xlab("Period") +
-  ylab("Fraction of false reports") + 
-  scale_x_continuous(breaks = 1:10 ) +
-  theme_minimal_hgrid()
+  summarise(prop_false_report_r1 = mean(false_report))
+#join both porportion in a single table
+period_prop_false_report_table <-
+full_join(period_prop_false_report, 
+          period_prop_false_report_r1) %>% 
+  rename(all_rounds = prop_false_report,
+         round_1 = prop_false_report_r1) %>% 
+  pivot_longer(-period, 
+               names_to = "rounds", 
+               values_to = "prop_false_report")
 
-ggsave("results/fig2A_one_round.pdf")
+period_prop_false_report_table %>% 
+  ggplot() + 
+  geom_line(aes(x = period,
+                y = prop_false_report,
+                color = rounds, 
+                group = rounds)) +
+  ylim(0,1) +
+  labs(
+    title = "Comparisson with the first round",
+    x = "Period", 
+    y = "Fraction of false reports") + 
+  scale_x_continuous(breaks = 1:10 ) +
+  theme_minimal_hgrid() +
+  scale_color_discrete(name = "Rounds", 
+                      breaks = c("all_rounds", 
+                                "round_1"),
+                      labels = c("All Rounds",
+                                 "Round 1")) 
+
+
+ggsave("results/fig2A_round_one.pdf")
 
 ## figure 2B ----
+prior_prop_false_report_r1 <-
 bsr_info %>% 
   filter(order_prior == 1) %>% 
   group_by(pur) %>% 
-  summarise(prop_false_report = mean(false_report))%>% 
-  ggplot() + 
-  geom_col(aes(x = factor(pur),y = prop_false_report)) +
-  ylim(0,1) +
-  xlab("Known prior of Red Urn") +
-  ylab("Fraction of false reports") +
-  theme_minimal_hgrid()
+  summarise(prop_false_report_r1 = mean(false_report))
+#join both porportion in a single table
+prior_prop_false_report_table <-
+  full_join(prior_prop_false_report, 
+            prior_prop_false_report_r1) %>% 
+  rename(all_rounds = prop_false_report,
+         round_1 = prop_false_report_r1) %>% 
+  pivot_longer(-pur, 
+               names_to = "rounds", 
+               values_to = "prop_false_report")
 
-ggsave("results/fig2B_one_round.pdf")
+prior_prop_false_report_table %>% 
+  ggplot() + 
+  geom_col(aes(x = factor(pur),
+               y = prop_false_report,
+               fill = rounds),
+           position=position_dodge()) +
+  ylim(0,1) +
+  labs(
+    title = "Comparisson with the first round",
+    x = "Known prior of Red Urn",
+    y = "Fraction of false reports") +
+  theme_minimal_hgrid() +
+  scale_fill_discrete(name = "Rounds", 
+                       breaks = c("all_rounds", 
+                                  "round_1"),
+                       labels = c("All Rounds",
+                                  "Round 1")) 
+
+ggsave("results/fig2B_round_one.pdf")
 
