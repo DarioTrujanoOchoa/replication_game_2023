d74425dd0a400b527e57085f587159d35c1b71ad
DarioTrujanoOchoa <dtrujano.22@gmail.com>
2024-04-22 10:40:29 -0700

    More figures including all the treatments

---
+++ figure2_all_treatments.R
@@ -0,0 +1,189 @@
+# Replication of Danz et al 2022 ----
+# Expanding fugure 2 to incude all the treatments
+# Dario Trujano-Ochoa
+
+rm(list=ls())
+
+library(pacman)
+p_load(tidyverse)
+p_load(ggplot2)
+p_load(cowplot)
+p_load(showtext)
+font_add_google("Schoolbell", "bell")
+
+# import data
+source("src/import_data.R")
+
+# calculate rates of false reports
+data.bsr.qsr <-
+  data.bsr.qsr %>%
+  mutate(false_report = (belief1 != pur))
+
+# All treatments ----
+treatment <- c("Information",
+               "RCL",
+               'No-Information',
+               "Feedback",
+               "Description")
+
+# Analysis by round within the proirs ----
+for (t in 1:length(treatment)) {
+  # data for BSR (elicitation method) and RLC treatment
+  bsr <-
+    data.bsr.qsr %>% 
+    filter(scoringrule == 1, 
+           treatment == t)
+  # there are 59 participant in BSR-rlc condition
+  bsr %>% distinct(subjectid)
+  
+  # Learning for the 50% prior ----
+  
+  ## Assign the relative presentation order within each prior considered. ----
+  bsr <- 
+    bsr %>% 
+    group_by(subjectid,pur) %>% 
+    arrange(subjectid, period) %>% 
+    mutate(order_prior = 1:n()) 
+  
+  # The number of rounds per prior is different for each prior.
+  # 50% has a larger number of repetitions.
+  bsr %>% 
+    group_by(subjectid, pur) %>% 
+    summarise(n_rounds = n()) %>% 
+    group_by(pur) %>% 
+    summarise(N_rounds = mean(n_rounds), sd(n_rounds))
+  
+  # Proportion of false report for the prior 50% across order of presentation
+  # There was no trend, it seems that the false report rate is constant.
+  
+  bsr %>% 
+    group_by(order_prior,pur) %>% 
+    summarise(prop_false_report = mean(false_report)) %>% 
+    ggplot(aes(x = order_prior,
+               y = prop_false_report,
+               color = factor(pur))) + 
+    geom_line() + geom_point(size = 2) +
+    ylim(0,1) +
+    theme_minimal_hgrid() +
+    labs(
+      title = paste(treatment[t]),
+      x = "Round", 
+      y = "Fraction of false reports", family = "bell") +
+    scale_color_discrete(name = "Prior") 
+  
+  ggsave(paste("results/rounds_all_priors_", treatment[t],".pdf",sep = ""),
+         width=6, height=5, pointsize = 5)
+  
+  # Prior Belief elicited.
+  # The prior belief should be around 50, however, there is clear trend downwards
+  bsr %>% 
+    group_by(order_prior,pur) %>% 
+    summarise(prior_stated = mean(belief1)) %>% 
+    ggplot(aes(x = order_prior,y = prior_stated,
+               color = factor(pur))) + 
+    geom_line() + geom_point() +
+    ylim(20,80) +
+    theme_minimal_hgrid() +
+    labs(
+      title = paste(treatment[t]),
+      x = "Round", 
+      y = "Stated Prior") +
+    scale_color_discrete(name = "Prior") 
+  
+  ggsave(paste("results/rounds_stated_prior_", treatment[t],".pdf",sep = ""),
+         width=6, height=5, pointsize = 5)
+
+  
+  # Filter for the first round per prior ----
+  
+  ## figure 2A ----
+  # original data
+  period_prop_false_report <-
+    bsr %>% 
+    group_by(period) %>% 
+    summarise(prop_false_report = mean(false_report))
+  # filter the first period
+  period_prop_false_report_r1 <-
+    bsr %>% 
+    filter(order_prior == 1) %>% 
+    group_by(period) %>% 
+    summarise(prop_false_report_r1 = mean(false_report))
+  #join both proportion in a single table
+  period_prop_false_report_table <-
+    full_join(period_prop_false_report, 
+              period_prop_false_report_r1) %>% 
+    rename(all_rounds = prop_false_report,
+           round_1 = prop_false_report_r1) %>% 
+    pivot_longer(-period, 
+                 names_to = "rounds", 
+                 values_to = "prop_false_report")
+  
+  period_prop_false_report_table %>% 
+    ggplot() + 
+    geom_line(aes(x = period,
+                  y = prop_false_report,
+                  color = rounds, 
+                  group = rounds)) +
+    ylim(0,1) +
+    labs(
+      title = paste(treatment[t]),
+      x = "Period", 
+      y = "Fraction of false reports") + 
+    scale_x_continuous(breaks = 1:10 ) +
+    theme_minimal_hgrid() +
+    scale_color_discrete(name = "Rounds", 
+                         breaks = c("all_rounds", 
+                                    "round_1"),
+                         labels = c("All Rounds",
+                                    "Round 1")) 
+  
+  ggsave(paste("results/fig2A_round_one_", treatment[t],".pdf",sep = ""),
+         width=s*g, height=s, pointsize = s)
+  
+  ## figure 2B ----
+  # original data
+  prior_prop_false_report <- 
+    bsr %>% 
+    group_by(pur) %>% 
+    summarise(prop_false_report = mean(false_report))
+  # filter the first period
+  prior_prop_false_report_r1 <-
+    bsr %>% 
+    filter(order_prior == 1) %>% 
+    group_by(pur) %>% 
+    summarise(prop_false_report_r1 = mean(false_report))
+  #join both porportion in a single table
+  prior_prop_false_report_table <-
+    full_join(prior_prop_false_report, 
+              prior_prop_false_report_r1) %>% 
+    rename(all_rounds = prop_false_report,
+           round_1 = prop_false_report_r1) %>% 
+    pivot_longer(-pur, 
+                 names_to = "rounds", 
+                 values_to = "prop_false_report")
+  
+  prior_prop_false_report_table %>% 
+    ggplot() + 
+    geom_col(aes(x = factor(pur),
+                 y = prop_false_report,
+                 fill = rounds),
+             position=position_dodge()) +
+    ylim(0,1) +
+    labs(
+      title = paste(treatment[t]),
+      x = "Known prior of Red Urn",
+      y = "Fraction of false reports") +
+    theme_minimal_hgrid() +
+    scale_fill_discrete(name = "Rounds", 
+                        breaks = c("all_rounds", 
+                                   "round_1"),
+                        labels = c("All Rounds",
+                                   "Round 1")) 
+  
+  ggsave(paste("results/fig2B_round_one_", treatment[t],".pdf",sep = ""),
+         width=s*g, height=s, pointsize = s)
+  
+  
+}
+
+
